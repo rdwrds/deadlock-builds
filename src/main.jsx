338fdc089b98dc4de717d5d2472bb47f80499b3a
rdwrds <r.edwards1492@gmail.com>
2024-10-06 17:37:26 -0500

    added context / components

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Outer from './components/Outer';
-import Header from './components/Header';
+import { useState} from "react";
+import { createRoot } from "react-dom/client";
+import Outer from "./components/Outer";
+import Header from "./components/Header";
 import DefaultInventory from "./components/DefaultInventory";
+import { TabContext } from "./components/TabContext";
+import { createGlobalStyle } from "styled-components";
+import { colors, getCurrentStyle } from "./Colors";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Header/>
-    <Outer/>
-    <div class="inventory">
-      <DefaultInventory/>
-    </div>
-  </StrictMode>,
-)
+const Main = () => {
+  const inventory = {
+    weapon: [],
+    spirit: [],
+    vitality: [], 
+    flex: [],
+  }
+  const [currentBuild, setCurrentBuild] = useState(inventory);
+  const [currentTab, setCurrentTab] = useState("weapon");
+
+  console.log(currentBuild)
+
+  return (
+    <>
+      <Header />
+      <TabContext.Provider
+        value={{currentBuild, setCurrentBuild, currentTab, setCurrentTab, getCurrentStyle }}
+      >
+        <Outer />
+        <div className="inventory">
+          <DefaultInventory type="weapon"/>
+          <DefaultInventory type="vitality"/>
+          <DefaultInventory type="spirit"/>
+          <DefaultInventory type="flex"/>
+        </div>
+      </TabContext.Provider>
+    </>
+  );
+};
+
+createRoot(document.getElementById("root")).render(<Main/>);
