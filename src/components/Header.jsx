5be0696f4f9d75e6001eebf8994afdbe2034f516
rdwrds <r.edwards1492@gmail.com>
2024-10-06 17:41:20 -0500

    removed test button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,6 @@
-import {items} from "../sorted_deadlock_items_condis.js";
-
 const Header = () => {
-  const handleButtonClick = () => {
-    console.log(items.Weapon[1].Basic_Magazine)
-  };
-
   return (
     <div className="header">
-      <button onClick={handleButtonClick}>data</button>
     </div>
   );
 };
