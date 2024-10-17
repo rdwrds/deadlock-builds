9c7f29a8bc0321a858487e313022f44f9949e98b
rdwrds <r.edwards1492@gmail.com>
2024-10-17 15:22:25 -0500

    added shades and handling

--- src/colors.js
+++ src/colors.js
@@ -1,27 +1,33 @@
 export const colors = {
-    textBackground: "#f0e1cb",
-    defaultBackground: "#28271e",
-    tanBackground: "#ccbda7",
-    greyBackground: "#525659",
-    soulsColor: "#5b8880",
-    weaponColor: "#e09b2f",
-    spiritColor: "#ce91fe",
-    vitalityColor: "#84b430",
-  };
+  textBackground: "#f0e1cb",
+  defaultBackground: "#28271e",
+  tanBackground: "#ccbda7",
+  greyBackground: "#525659",
+  soulsColor: "#5b8880",
+  weapon500: "#e09b2f",
+  weapon600: "#b37c26",
+  weapon700: "#865d1c",
+  spirit500: "#ce91fe",
+  spirit600: "#a574cb",
+  spirit700: "#7c5798",
+  vitality500: "#84b430",
+  vitality600: "#6a9026",
+  vitality700: "#4f6c1d",
+};
 
-export const getCurrentStyle = (currentTab) => {
-    switch (currentTab) {
-      case "weapon":
-        return {backgroundColor: colors.weaponColor};
-        break;
-      case "spirit":
-        return {backgroundColor: colors.spiritColor};
-        break;
-      case "vitality":
-        return {backgroundColor: colors.vitalityColor};
-        break;
-      default:
-        return {backgroundColor: colors.weaponColor};
-        break;
-    }
+export const getCurrentStyle = (currentTab, shade) => {
+  switch (currentTab) {
+    case "weapon":
+      return { backgroundColor: colors[`weapon${shade || 500}`] };
+      break;
+    case "spirit":
+      return { backgroundColor: colors[`spirit${shade || 500}`] };
+      break;
+    case "vitality":
+      return { backgroundColor: colors[`vitality${shade || 500}`] };
+      break;
+    default:
+      return { backgroundColor: colors.greyBackground };
+      break;
   }
+};
