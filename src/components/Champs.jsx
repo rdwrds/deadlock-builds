ff2d5269df6f1b7bba7bbd828525702c18596b74
rdwrds <r.edwards1492@gmail.com>
2024-10-17 15:27:30 -0500

    added functionality

--- src/components/Champs.jsx
+++ src/components/Champs.jsx
@@ -1,11 +1,76 @@
+import { TabContext } from "./TabContext";
+import { useContext } from "react";
+
 const Champs = () => {
+  const champs_names = [
+    "Abrams",
+    "Bebop",
+    "Dynamo",
+    "Grey_Talon",
+    "Haze",
+    "Infernus",
+    "Ivy",
+    "Kelvin",
+    "Lady_Geist",
+    "Lash",
+    "McGinnis",
+    "Mirage",
+    "Mo_&_Krill",
+    "Paradox",
+    "Pocket",
+    "Seven",
+    "Shiv",
+    "Vindicta",
+    "Viscous",
+    "Warden",
+    "Wraith",
+    "Yamato",
+  ];
+
+  const { currentBuild, setCurrentBuild } = useContext(TabContext);
+
+  const getImgUrl = (name) => {
+    let ext = ".png"; // can be anything
+    const imgUrl = new URL(`../assets/champs/${name}_card.png`, import.meta.url)
+      .href;
+    return imgUrl;
+  };
+
+  const setCurrentChamp = (name) => {
+    if (currentBuild["champ"] === name) {
+      setCurrentBuild({
+        ...currentBuild,
+        champ: "",
+      });
+      return;
+    }
+
+    setCurrentBuild({
+      ...currentBuild,
+      champ: name,
+    });
+  };
+
   return (
     <div className="champs">
-      <div className="champ"></div>
-      <div className="champ"></div>
-      <div className="champ"></div>
-      <div className="champ"></div>
-      <div className="champ"></div>
+      {champs_names.map((name) => {
+        const op = currentBuild["champ"] === name ? 0.3 : 1;
+        return (
+          <div
+            className="champ"
+            onClick={() => {
+              setCurrentChamp(name);
+            }}
+            style={{ opacity: op }}
+            key={name}
+          >
+            <img src={getImgUrl(name)} alt={name} />
+            <h3 className="champ-label">
+              {name.replaceAll("_", " ").toUpperCase()}
+            </h3>
+          </div>
+        );
+      })}
     </div>
   );
 };
