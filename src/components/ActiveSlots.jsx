812ac27d3007fa672ba76c0942255bf6b5223270
rdwrds <r.edwards1492@gmail.com>
2024-10-17 15:26:33 -0500

    added functionality

--- src/components/ActiveSlots.jsx
+++ src/components/ActiveSlots.jsx
@@ -1,10 +1,59 @@
+import { useContext } from "react";
+import { TabContext } from "./TabContext";
+import { colors } from "../Colors";
+
 const ActiveSlots = () => {
+  const {currentBuild, getCurrentStyle} = useContext(TabContext);
+  
+  const getActiveItems = () => {
+    const weaponActives = currentBuild["weapon"].filter((item)=>item?.["Activation"] !== "Passive");
+    const spiritActives = currentBuild["spirit"].filter((item)=>item?.["Activation"] !== "Passive");
+    const vitalityActives = currentBuild["vitality"].filter((item)=>item?.["Activation"] !== "Passive");
+    const flexActives = currentBuild["flex"].filter((item)=>item?.["Activation"] !== "Passive");
+  
+    return [
+      ...weaponActives,
+      ...spiritActives,
+      ...vitalityActives,
+      ...flexActives
+    ]
+  }
+
+  const romanNumerals = [
+    "I",
+    "II",
+    "III",
+    "IV"
+  ]
+
+  console.log(getActiveItems())
+  const currentItems = getActiveItems();
+
+  const getImgUrl = (name) => {
+    let ext = ".png"; // can be anything
+    const imgUrl = new URL(`../assets/items/${name}.png`, import.meta.url).href;
+    return imgUrl;
+  };
+
+  let images = [];
+  for (let i = 0; i < 4; i += 1) {
+    images.push(getImgUrl(currentItems?.[i]?.["name"] || "placeholder"));
+  }
+
   return (
-    <div className="active-slots">
-      <div className="active-item"></div>
-      <div className="active-item"></div>
-      <div className="active-item"></div>
-      <div className="active-item"></div>
+    <div className="active-inventory">
+      {
+        images.map((img, index) => {
+          const tier = currentItems?.[index]?.["Tier"]
+          const slot = currentItems?.[index]?.["Slot"]
+          return (
+            <div className="active-slots" style={getCurrentStyle(currentItems[index]?.["Slot"])}>
+              <h1 className="tier-icon" style={{color: colors[`${slot}500`]}}>{romanNumerals[tier-1]}</h1>
+              <img src={img}></img>
+            </div>
+          );
+        })
+      }
     </div>
   );
 };
