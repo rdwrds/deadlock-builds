1fe42dc7705fa2354077e3749a9f5fd6612f1092
rdwrds <r.edwards1492@gmail.com>
2024-10-17 15:32:07 -0500

    added champ preview

--- src/components/Outer.jsx
+++ src/components/Outer.jsx
@@ -1,16 +1,44 @@
 import { useState } from "react";
+import { useContext } from "react";
 import Items from "./Items";
 import Tabs from "./Tabs";
 import Champs from "./Champs";
+import { TabContext } from "./TabContext";
+import champ_placeholder from "../assets/champs/champ_placeholder.png";
 
 const Outer = () => {
+  const { currentBuild } = useContext(TabContext);
+
+  const getImgUrl = (name) => {
+    let ext = ".png"; // can be anything
+    const imgUrl = new URL(`../assets/champs/${name}_card.png`, import.meta.url)
+      .href;
+    return imgUrl;
+  };
+
+  let currentChampPreview = currentBuild["champ"]
+    ? getImgUrl(currentBuild["champ"])
+    : champ_placeholder;
+
   return (
     <div className="outer">
-      <div className="item-tabs-container" >
-          <Tabs />
-          <Items />
+      <div className="item-tabs-container">
+        <Tabs />
+        <Items />
+      </div>
+      <div className="champs-wrapper">
+        <div className="build-info">
+          <div className="current-champ">
+            <img src={currentChampPreview} alt="" />
+            <h4 className="champ-label">{currentBuild["champ"].toUpperCase()}</h4>
+          </div>
+          <div className="build-stats">
+            
+            <p>build cost</p>
+          </div>
         </div>
         <Champs />
+      </div>
     </div>
   );
 };
