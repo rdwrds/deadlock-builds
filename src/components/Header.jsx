81f879aaf916121e0d7350193de55d7bd510c729
rdwrds <r.edwards1492@gmail.com>
2024-10-21 14:33:52 -0500

    added home navlink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
     <div className="header">
+      <NavLink className="nav-link" to="/">
+        Home
+      </NavLink>
     </div>
   );
 };
