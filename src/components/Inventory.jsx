f16405a13ddc4f473bd662daba34032cfe0054b1
rdwrds <r.edwards1492@gmail.com>
2024-10-21 15:24:16 -0500

    renamed to 'inventory'

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,6 +1,38 @@
-const Inventory = () => {
+import { useContext, useEffect } from "react";
+import { TabContext } from "./TabContext";
+import { colors, getCurrentStyle } from "../Colors";
+
+const Inventory = ({ type }) => {
+  const { currentBuild } = useContext(TabContext);
+
+  const romanNumerals = ["I", "II", "III", "IV"];
+
+  const getImgUrl = (name) => {
+    let ext = ".png"; // can be anything
+    const imgUrl = new URL(`../assets/items/${name}.png`, import.meta.url).href;
+    return imgUrl;
+  };
+
+  let images = [];
+  for (let i = 0; i < 4; i += 1) {
+    images.push(getImgUrl(currentBuild[type]?.[i]?.["name"] || "placeholder"));
+  }
+
   return (
-    <div className="inventory"></div>
-  )
-}
-export default Inventory
+    <div className={`${type}-inventory`}>
+      {images.map((img, index) => {
+        const tier = currentBuild[type]?.[index]?.["Tier"];
+        const slot = currentBuild[type]?.[index]?.["Slot"];
+        return (
+          <div className={`${type}-slots`} style={getCurrentStyle(slot)}>
+            <h1 className="tier-icon" style={{ color: colors[`${slot}500`] }}>
+              {romanNumerals[tier - 1]}
+            </h1>
+            <img src={img}></img>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default Inventory;
