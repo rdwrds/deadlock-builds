dbd7d02baf40ea715d2c0468ebef931c4bb8588a
rdwrds <r.edwards1492@gmail.com>
2024-10-21 16:06:20 -0500

    init

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import { TabContext } from "./TabContext";
+
+export {default as Outer} from  "./Outer/Outer";
+export {default as Items} from  "./Items/Items";
+export {default as Item} from  "./Item/Item";
+export {default as Tabs} from  "./Tabs/Tabs";
+export {default as Champs} from  "./Champs/Champs";
+export {default as TierGrid} from  "./TierGrid/TierGrid";
+export {default as TierLabel} from  "./TierLabel/TierLabel";
+export {default as HoverBody} from  "./HoverBody/HoverBody";
+export {default as ActiveSlots} from  "./ActiveSlots/ActiveSlots";
+export {default as Inventory} from  "./Inventory/Inventory";
+export {TabContext} from  "./TabContext";
